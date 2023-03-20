59f014ca3b3caa49b015aba12a06878d90c1bfb6
Keks <keks@htmlacademy.ru>
2023-03-20 10:52:27 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/data.js
@@ -0,0 +1,60 @@
+import { getRandomInteger, createRandomIdGenerator } from './util.js';
+
+const LIKE_MIN_COUNT = 15;
+const LIKE_MAX_COUNT = 200;
+const AVATAR_MIN_COUNT = 1;
+const AVATAR_MAX_COUNT = 6;
+const SIMILAR_POST_COUNT = 25;
+const NAMES = [
+  'Павел',
+  'Артём',
+  'Иван',
+  'Илья',
+  'Александр',
+  'Никита',
+  'Алексей',
+];
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+const DESCRIPTIONS = [
+  'Я не верю в будущее, я верю в сегодня',
+  'Если жизнь подкидывает мне лимоны, я делаю из них лимонад',
+  'На 80% состою из важных дел',
+  'Моя жизнь – мои правила',
+  'Я не везунчик, я просто талантливый',
+  'Я уверен в себе, и мнение окружающих не заставит меня передумать',
+  'Мой девиз – быть экстраординарной',
+];
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+const generateId = createRandomIdGenerator(1, 25);
+const generateUrl = createRandomIdGenerator(1, 25);
+const generateIdComments = createRandomIdGenerator(1, 500);
+
+const createComment = () => ({
+  id: generateIdComments(),
+  avatar: `img/avatar-${getRandomInteger(AVATAR_MIN_COUNT, AVATAR_MAX_COUNT)}.svg`,
+  message: `${getRandomArrayElement(MESSAGES)}`,
+  name: `${getRandomArrayElement(NAMES)}`,
+});
+
+const createArrayComments = () => Array.from({ length: getRandomInteger (1, 10) }, () => createComment());
+
+const createPostPhotos = () => ({
+  id: generateId(),
+  url: `photos/${generateUrl()}.jpg`,
+  description: `${getRandomArrayElement(DESCRIPTIONS)}`,
+  likes: getRandomInteger(LIKE_MIN_COUNT, LIKE_MAX_COUNT),
+  comments: createArrayComments(),
+});
+
+const similarPost = () => Array.from({ length: SIMILAR_POST_COUNT }, createPostPhotos);
+
+export { similarPost };
---
+++ 8/js/miniatures-full.js
@@ -0,0 +1,69 @@
+import { isEscapeKey, isEnterKey } from './util.js';
+import { createComment, loadComments } from './minuatures-comment.js';
+import { descriptionData } from './main.js';
+
+const picturesBlock = document.querySelector('.pictures');
+const bigPicturesContainer = document.querySelector('.big-picture');
+const bigPicturesImg = bigPicturesContainer.querySelector('.big-picture__img').querySelector('img');
+const bigPicturesClose = bigPicturesContainer.querySelector('.big-picture__cancel');
+const bigPicturesLikes = bigPicturesContainer.querySelector('.likes-count');
+const bigPicturesComments = bigPicturesContainer.querySelector('.comments-count');
+const picturesCaption = bigPicturesContainer.querySelector('.social__caption');
+const socialComments = bigPicturesContainer.querySelector('.social__comments');
+const moreComments = bigPicturesContainer.querySelector('.comments-loader');
+
+const onClickPictures = (evt) => {
+  evt.preventDefault();
+  const target = evt.target.closest('a');
+  if (target) {
+    const correctDescription = descriptionData.find((item) => item.id === Number(target.dataset.id));
+    bigPicturesContainer.classList.remove('hidden');
+    moreComments.classList.remove('hidden');
+
+    bigPicturesImg.src = correctDescription.url;
+    bigPicturesImg.alt = correctDescription.description;
+    picturesCaption.textContent = correctDescription.description;
+    bigPicturesLikes.textContent = correctDescription.likes;
+    bigPicturesComments.textContent = correctDescription.comments.length;
+    socialComments.innerHTML = '';
+
+    document.body.classList.add('modal-open');
+
+    createComment(correctDescription.comments, socialComments);
+    loadComments();
+  }
+};
+
+picturesBlock.addEventListener('click', onClickPictures);
+
+moreComments.addEventListener('click', loadComments);
+
+const onClosePictures = () => {
+  bigPicturesContainer.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+};
+
+bigPicturesClose.addEventListener('click', () => {
+  onClosePictures();
+  document.body.classList.remove('modal-open');
+  moreComments.classList.remove('hidden');
+});
+
+bigPicturesClose.addEventListener('keydown', (evt) => {
+  if (isEnterKey(evt)) {
+    onClosePictures();
+  }
+  document.body.classList.remove('modal-open');
+  moreComments.classList.remove('hidden');
+});
+
+
+document.addEventListener('keydown', (evt) => {
+  if (isEscapeKey(evt)) {
+    bigPicturesContainer.classList.add('hidden');
+  }
+  document.body.classList.remove('modal-open');
+  moreComments.classList.remove('hidden');
+});
+
+export { onClickPictures };
