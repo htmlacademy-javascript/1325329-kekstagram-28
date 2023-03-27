11c92fe1936b6a450a2370edd7522eb3fc5a8d1d
Keks <keks@htmlacademy.ru>
2023-03-27 09:09:09 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,12 @@
+import { similarPost } from './data.js';
+import { renderingMiniatures } from './miniatures-mini.js';
+import './miniatures-full.js';
+import './upload.js';
+import './validate.js';
+import './scale.js';
+import './slider.js';
+
+const descriptionData = similarPost();
+renderingMiniatures(descriptionData);
+
+export {descriptionData};
