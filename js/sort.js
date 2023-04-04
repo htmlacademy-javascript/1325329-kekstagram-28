b970ec0d4ab2c7074c30479adc562df4a19530a7
Alexander96196 <chesnokov.alexander@mail.ru>
2023-04-04 11:44:22 +0500

    Сделали сортировку и убрали дребезг

--- js/sort.js
+++ js/sort.js
@@ -1,4 +1,5 @@
 const PICTURES_RANDOM_COUNT = 10;
+const SORT_RANDOM_NUMBER = 0.5;
 const Sort = {
   DEFAULT: 'filter-default',
   RANDOM: 'filter-random',
@@ -6,44 +7,39 @@ const Sort = {
 };
 
 const sortElement = document.querySelector('.img-filters');
-let currentSort = Sort.DEFAULT;
-let pictures = [];
+const sortForm = document.querySelector('.img-filters__form');
+const sortBtn = sortForm.querySelectorAll('.img-filters__button');
 
-const randomSort = () => Math.random() - 0.5;
+let currentSort = Sort.DEFAULT;
+let descriptionDataSort = [];
+
+const randomSort = () => Math.random() - SORT_RANDOM_NUMBER;
 
 const sortByDiscussed = (a, b) => b.comments.length - a.comments.length;
 
 const getSortedPictures = () => {
   switch (currentSort) {
     case Sort.RANDOM:
-      return [...pictures].sort(randomSort).slice(0, PICTURES_RANDOM_COUNT);
+      return [...descriptionDataSort].sort(randomSort).slice(0, PICTURES_RANDOM_COUNT);
     case Sort.DISCUSSED:
-      return [...pictures].sort(sortByDiscussed);
+      return [...descriptionDataSort].sort(sortByDiscussed);
     default:
-      return [...pictures];
+      return [...descriptionDataSort];
   }
 };
 
 const onSortClick = (cb) => {
-  sortElement.addEventListener('click', (evt) => {
-    if (!evt.target.classList.contains('img-filters__button')) {
-      return;
-    }
-    const clickButton = evt.target;
-    if (clickButton.id === currentSort) {
-      return;
-    }
-    sortElement.querySelector('.img-filters__button--active');
-    sortElement.classList.remove('.img-filters__button--active');
-    clickButton.classList.add('.img-filters__button--active');
-    currentSort = clickButton.id;
+  sortForm.addEventListener('click', (evt) => {
+    sortBtn.forEach((item) => item.classList.remove('img-filters__button--active'));
+    evt.target.classList.add('img-filters__button--active');
+    currentSort = evt.target.id;
     cb(getSortedPictures());
   });
 };
 
 const init = (loadedPictures, cb) => {
-  sortElement.classList.remove('.img-filters--inactive');
-  pictures = [...loadedPictures];
+  sortElement.classList.remove('img-filters--inactive');
+  descriptionDataSort = [...loadedPictures];
   onSortClick(cb);
 };
 
